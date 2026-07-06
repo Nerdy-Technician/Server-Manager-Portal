a5c48fbe07c6a59329b285661dc09faa3577b1f0
jl94x4 <jasonlucas58@gmail.com>
2026-07-06 18:44:40 +0100

    Fix Uncaught ReferenceError: useRef is not defined

--- client/App.tsx
+++ client/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import { SettingsDashboard } from './settings/SettingsDashboard';
 import { bindAppConfirm } from './shared/confirm';
 import { apiFetch } from './shared/api';
